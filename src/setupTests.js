dba3e78933b04ef2857a73d4f2c957175281ba68
unknown <kantamanenibharath21@gmail.com>
2023-06-24 17:11:17 +0530

    adding empty react project to git repository

---
+++ src/setupTests.js
@@ -0,0 +1,23 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+\\]
+';
+
+
+
+
+
+
+
+
+
+
+';;;;;;;;;p;]]]]]]]]]/[
+    ,,,,,,,,,pl[
+        '[,m
+            
+            p[',l[,
+                [,o
+                    llllmk';// learn more: https://github.com/\testing-library/jest-dom
+import '@testing-library/jest-dom';
